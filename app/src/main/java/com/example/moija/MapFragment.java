d4910afdfee109959ba4c830394c6cbfe4823a7a
sunRoot-Im <dlagorms1@gmail.com>
2023-09-20 17:31:32 +0900

    버튼 클릭 시 색깔 변함
    채팅 / 지도 클릭 시 색깔 변환
    색깔은 계속 바꿔 나갈듯

--- app/src/main/java/com/example/moija/MapFragment.java
+++ app/src/main/java/com/example/moija/MapFragment.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
+import com.kakao.vectormap.MapView;
+
 public class MapFragment extends Fragment {
 
     public MapFragment() {

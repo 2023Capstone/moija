076fc5fc1a62dc5c3e6e191ae4911f1327840dbb
sunRoot-Im <dlagorms1@gmail.com>
2023-09-25 20:41:33 +0900

    ㅈㄷ
    ㅈㄷ

--- app/src/main/java/com/example/moija/MapFragment.java
+++ app/src/main/java/com/example/moija/MapFragment.java
@@ -4,20 +4,69 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
+import com.kakao.vectormap.KakaoMap;
+import com.kakao.vectormap.KakaoMapReadyCallback;
+import com.kakao.vectormap.LatLng;
+import com.kakao.vectormap.MapType;
 import com.kakao.vectormap.MapView;
+import com.kakao.vectormap.MapViewInfo;
 
-public class MapFragment extends Fragment {
-
-    public MapFragment() {
-        // Required empty public constructor
-    }
+public class MapFragment extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_map_fragment, container, false);
+        View view = inflater.inflate(R.layout.activity_map_fragment, container, false);
+
+        MapView mapView = view.findViewById(R.id.map_view);
+
+
+        mapView.start(new KakaoMapReadyCallback() {
+            @Override
+            public void onMapReady(KakaoMap kakaoMap) {
+
+            }
+
+            @Override
+            public LatLng getPosition() {
+                // 지도 시작 시 위치 좌표를 설정
+                return LatLng.from(37.406960, 127.115587);
+            }
+
+            @Override
+            public int getZoomLevel() {
+                // 지도 시작 시 확대/축소 줌 레벨 설정
+                return 15;
+            }
+
+
+            @Override
+            public String getViewName() {
+                // KakaoMap 의 고유한 이름을 설정
+                return "MyFirstMap";
+            }
+
+            @Override
+            public boolean isVisible() {
+                // 지도 시작 시 visible 여부를 설정
+                return true;
+            }
+
+            @Override
+            public String getTag() {
+                // KakaoMap 의 tag 을 설정
+                return "FirstMapTag";
+            }
+
+
+
+        });
+
+
+        return view;
     }
 }

aa53ea288ef17e5fa96dc8fd70321312ebb085d6
sunRoot-Im <dlagorms1@gmail.com>
2023-09-26 11:13:05 +0900

    수정
    수정

--- app/src/main/java/com/example/moija/MapFragment.java
+++ app/src/main/java/com/example/moija/MapFragment.java
@@ -1,5 +1,10 @@
 package com.example.moija;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,25 +12,37 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.kakao.sdk.template.model.Button;
+import com.kakao.vectormap.MapView;
 import com.kakao.vectormap.KakaoMap;
 import com.kakao.vectormap.KakaoMapReadyCallback;
 import com.kakao.vectormap.LatLng;
+import com.kakao.sdk.template.model.Button;
+import com.kakao.sdk.template.model.Button;
 import com.kakao.vectormap.MapType;
 import com.kakao.vectormap.MapView;
 import com.kakao.vectormap.MapViewInfo;
+import com.kakao.vectormap.mapwidget.MapWidget;
+import com.kakao.vectormap.mapwidget.MapWidgetManager;
 
-public class MapFragment extends Fragment{
+public class MapFragment extends Fragment {
+    private MapView mapView;
+    private Button kakaoButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.activity_map_fragment, container, false);
 
-        MapView mapView = view.findViewById(R.id.map_view);
+        mapView = view.findViewById(R.id.map_view);
 
+//        kakaoButton = view.findViewById(R.id.floating_button);
 
         mapView.start(new KakaoMapReadyCallback() {
+            LocationData locationData = new LocationData();
+
+
             @Override
             public void onMapReady(KakaoMap kakaoMap) {
 
@@ -34,38 +51,29 @@ public class MapFragment extends Fragment{
             @Override
             public LatLng getPosition() {
                 // 지도 시작 시 위치 좌표를 설정
-                return LatLng.from(37.406960, 127.115587);
+                return LatLng.from(locationData.getLatitude(), locationData.getLongitude());
             }
 
             @Override
             public int getZoomLevel() {
-                // 지도 시작 시 확대/축소 줌 레벨 설정
                 return 15;
             }
 
-
             @Override
             public String getViewName() {
-                // KakaoMap 의 고유한 이름을 설정
                 return "MyFirstMap";
             }
 
             @Override
             public boolean isVisible() {
-                // 지도 시작 시 visible 여부를 설정
                 return true;
             }
 
             @Override
             public String getTag() {
-                // KakaoMap 의 tag 을 설정
                 return "FirstMapTag";
             }
-
-
-
         });
-
 
         return view;
     }

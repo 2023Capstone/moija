d8417b52a840b408609c20ad20af673bee32fe98
hojin535 <123153695+hojin535@users.noreply.github.com>
2023-09-26 20:58:04 +0900

    ...
    ...

--- app/src/main/java/com/example/moija/MapFragment.java
+++ app/src/main/java/com/example/moija/MapFragment.java
@@ -16,6 +16,7 @@ import androidx.fragment.app.Fragment;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.type.LatLng;
 import com.kakao.sdk.template.model.Button;
 import com.kakao.vectormap.MapView;
 import com.kakao.vectormap.KakaoMap;

992fdf7bd141989c0111c3289090ff1cc11a86ac
sunRoot-Im <dlagorms1@gmail.com>
2023-09-26 11:44:56 +0900

    수정2
    ㅅ

--- app/src/main/java/com/example/moija/MapFragment.java
+++ app/src/main/java/com/example/moija/MapFragment.java
@@ -9,10 +9,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.location.Location;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.kakao.sdk.template.model.Button;
 import com.kakao.vectormap.MapView;
 import com.kakao.vectormap.KakaoMap;
@@ -28,7 +30,9 @@ import com.kakao.vectormap.mapwidget.MapWidgetManager;
 
 public class MapFragment extends Fragment {
     private MapView mapView;
-    private Button kakaoButton;
+    private android.widget.Button location_btn;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationData locationData = new LocationData(); // Initialize the LocationData object
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,12 +40,10 @@ public class MapFragment extends Fragment {
         View view = inflater.inflate(R.layout.activity_map_fragment, container, false);
 
         mapView = view.findViewById(R.id.map_view);
-
-//        kakaoButton = view.findViewById(R.id.floating_button);
+        location_btn = view.findViewById(R.id.location_btn);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
 
         mapView.start(new KakaoMapReadyCallback() {
-            LocationData locationData = new LocationData();
-
 
             @Override
             public void onMapReady(KakaoMap kakaoMap) {
@@ -72,6 +74,26 @@ public class MapFragment extends Fragment {
             @Override
             public String getTag() {
                 return "FirstMapTag";
+            }
+        });
+
+        location_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                    return;
+                }
+                fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                if (location != null) {
+                                    locationData.setLatitude(location.getLatitude());
+                                    locationData.setLongitude(location.getLongitude());
+                                }
+                            }
+                        });
             }
         });
 

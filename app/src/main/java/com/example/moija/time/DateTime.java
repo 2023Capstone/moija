f3b70303cf5223175519745dd5b45c0e0a436300
ENERGYMACARON <131278709+ENERGYMACARON@users.noreply.github.com>
2023-10-05 22:50:52 +0900

    Update DateTime.java

--- app/src/main/java/com/example/moija/time/DateTime.java
+++ app/src/main/java/com/example/moija/time/DateTime.java
@@ -8,7 +8,7 @@ import java.util.TimeZone;
 public class DateTime {
     public static String getCurrentDateTime() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
-        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul")); // 표준 시간대를 UTC로 설정
+        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul")); // 시간대를 설정
         return sdf.format(new Date());
     }
 }
